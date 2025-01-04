85ad0c6dacfedcb02f70914f0a52cf81da4acb35
MmmarRTHa <m27842n@outlook.com>
2025-01-03 19:20:18 -0800

    - update: feature/effort_liveview - add mount and render

--- lib/neighborly_web/live/effort_live.ex
+++ lib/neighborly_web/live/effort_live.ex
@@ -1,9 +1,30 @@
 defmodule NeighborlyWeb.EffortLive do
-    use NeighborlyWeb, :live_view
+  use NeighborlyWeb, :live_view
 
-    def mount(_params, _session, socket) do
-        socket = assign(socket, responders: 0, minutes_per_responder: 10)
-        {:ok, socket}
-    end
+  def mount(_params, _session, socket) do
+    socket = assign(socket, responders: 2, minutes_per_responder: 10)
+    {:ok, socket}
+  end
 
+  # render de liveview current state
+  def render(assigns) do
+    ~H"""
+    <div class="effort">
+      <h1>Community Love</h1>
+      <section>
+        <div>
+          {@responders}
+        </div>
+        &times
+        <div>
+          {@minutes_per_responder}
+        </div>
+        =
+        <div>
+        {@responders * @minutes_per_responder}
+        </div>
+      </section>
+    </div>
+    """
+  end
 end
