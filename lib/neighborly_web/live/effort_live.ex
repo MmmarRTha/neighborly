1eaeae2372650d09ba8fdb54215063e9d836064e
MmmarRTHa <m27842n@outlook.com>
2025-01-05 20:50:45 -0800

    - update: feature/effort_liveview - add a handle event for add responders in lv

--- lib/neighborly_web/live/effort_live.ex
+++ lib/neighborly_web/live/effort_live.ex
@@ -2,7 +2,7 @@ defmodule NeighborlyWeb.EffortLive do
   use NeighborlyWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, responders: 2, minutes_per_responder: 10)
+    socket = assign(socket, responders: 0, minutes_per_responder: 10)
     {:ok, socket}
   end
 
@@ -11,7 +11,11 @@ defmodule NeighborlyWeb.EffortLive do
     ~H"""
     <div class="effort">
       <h1>Community Love</h1>
+
       <section>
+        <button phx-click="add" phx-value-quantity="5">
+          +
+        </button>
         <div>
           {@responders}
         </div>
@@ -21,10 +25,18 @@ defmodule NeighborlyWeb.EffortLive do
         </div>
         =
         <div>
-        {@responders * @minutes_per_responder}
+          {@responders * @minutes_per_responder}
         </div>
       </section>
     </div>
     """
+  end
+
+  def handle_event("add", %{"quantity" => quantity}, socket) do
+    # responders = socket.assigns.responders + 1
+    # socket = assign(socket, :responders, responders)
+
+    socket = update(socket, :responders, &(&1 + String.to_integer(quantity)))
+    {:noreply, socket}
   end
 end
