b6d17c9fc0731901dfb76ef0654fca273523ad3a
MmmarRTHa <m27842n@outlook.com>
2025-01-07 19:41:11 -0800

    - update: feature/incidents - add function components to card and badge

--- lib/neighborly_web/live/incident_live/index.ex
+++ lib/neighborly_web/live/incident_live/index.ex
@@ -12,19 +12,33 @@ defmodule NeighborlyWeb.IncidentLive.Index do
     ~H"""
     <div class="incident-index">
       <div class="incidents">
-        <div :for={incident <- @incidents} class="card">
-          <img src={incident.image_path} alt="image" />
-          <h2>{incident.name}</h2>
-          <div class="details">
-            <div class="badge">
-              {incident.status}
-            </div>
-            <div class="priority">
-              {incident.priority}
-            </div>
-          </div>
+        <.incident_card :for={incident <- @incidents} incident={incident}/>
+      </div>
+    </div>
+    """
+  end
+
+  attr :incident, Neighborly.Incident, required: true
+  def incident_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@incident.image_path} alt="image" />
+      <h2>{@incident.name}</h2>
+      <div class="details">
+        <.badge status={@incident.status} />
+        <div class="priority">
+          {@incident.priority}
         </div>
       </div>
+    </div>
+    """
+  end
+
+  attr :status, :atom, required: true, values: [:pending, :resolved, :canceled]
+  def badge(assigns) do
+    ~H"""
+    <div class="inline-block px-2 py-1 text-xs font-medium uppercase border rounded-md text-lime-600 border-lime-600">
+      {@status}
     </div>
     """
   end
