f629d19bd54b4cb97873c40d4d90a0f45c151f25
MmmarRTHa <m27842n@outlook.com>
2025-02-10 22:38:02 -0800

    - update: feature/js_commands - add js commands for common client-side needs

--- lib/neighborly_web/live/admin_incident_live/index.ex
+++ lib/neighborly_web/live/admin_incident_live/index.ex
@@ -15,6 +15,19 @@ defmodule NeighborlyWeb.AdminIncidentLive.Index do
   def render(assigns) do
     ~H"""
     <div class="admin-index">
+      <.button phx-click={
+        JS.toggle(
+          to: "#joke",
+          in: {"ease-in-out duration-300", "opacity-0", "opacity-100"},
+          out: {"ease-in-out duration-300", "opacity-100", "opacity-0"},
+          time: 300
+        )
+      }>
+        Toggle Joke
+      </.button>
+      <div class="hidden joke" id="joke">
+        What's a tree's favorite drink?
+      </div>
       <.header>
         {@page_title}
         <:actions>
@@ -23,7 +36,11 @@ defmodule NeighborlyWeb.AdminIncidentLive.Index do
           </.link>
         </:actions>
       </.header>
-      <.table id="incidents" rows={@streams.incidents}>
+      <.table
+        id="incidents"
+        rows={@streams.incidents}
+        row_click={fn {_, incident} -> JS.navigate(~p"/incidents/#{incident}") end}
+      >
         <:col :let={{_dom_id, incident}} label="Name">
           <.link navigate={~p"/incidents/#{incident}"}>
             {incident.name}
@@ -40,10 +57,9 @@ defmodule NeighborlyWeb.AdminIncidentLive.Index do
             Edit
           </.link>
         </:action>
-        <:action :let={{_dom_id, incident}}>
+        <:action :let={{dom_id, incident}}>
           <.link
-            phx-click="delete"
-            phx-value-id={incident.id}
+            phx-click={delete_and_hide(dom_id, incident)}
             data-confirm="Do you want to delete it?"
           >
             <.icon name="hero-trash" class="w-4 h-4" /> Delete
@@ -59,5 +75,10 @@ defmodule NeighborlyWeb.AdminIncidentLive.Index do
     {:ok, _} = Admin.delete_incident(incident)
 
     {:noreply, stream_delete(socket, :incidents, incident)}
+  end
+
+  def delete_and_hide(dom_id, incident) do
+    JS.push("delete", value: %{id: incident.id})
+    |> JS.hide(to: "##{dom_id}", transition: "fade-out")
   end
 end
