7defdd3e1d63d960a119c67390dd1bfef8aceeb1
MmmarRTHa <m27842n@outlook.com>
2025-02-12 01:19:33 -0800

    - update: feature/live_scaffolding_phx_1_8 - add live generator for categories using phoenix 1.8 github main

---
+++ lib/neighborly_web/live/category_live/show.ex
@@ -0,0 +1,37 @@
+defmodule NeighborlyWeb.CategoryLive.Show do
+  use NeighborlyWeb, :live_view
+
+  alias Neighborly.Categories
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <.header>
+      Category {@category.id}
+      <:subtitle>This is a category record from your database.</:subtitle>
+      <:actions>
+        <.button phx-click={JS.dispatch("click", to: {:inner, "a"})}>
+          <.link navigate={~p"/categories/#{@category}/edit?return_to=show"}>
+            Edit category
+          </.link>
+        </.button>
+      </:actions>
+    </.header>
+
+    <.list>
+      <:item title="Name">{@category.name}</:item>
+      <:item title="Slug">{@category.slug}</:item>
+    </.list>
+
+    <.back navigate={~p"/categories"}>Back to categories</.back>
+    """
+  end
+
+  @impl true
+  def mount(%{"id" => id}, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:page_title, "Show Category")
+     |> assign(:category, Categories.get_category!(id))}
+  end
+end
