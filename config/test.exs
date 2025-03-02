02aa665eddd8eb7e1592edf9c8444c0268fee1d0
MmmarRTHa <m27842n@outlook.com>
2025-03-01 17:13:35 -0800

    - update: main - fix config tests conn

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :neighborly, Neighborly.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "backend_elixir",
+  password: "postgres_p0p",
   hostname: "localhost",
   database: "neighborly_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
