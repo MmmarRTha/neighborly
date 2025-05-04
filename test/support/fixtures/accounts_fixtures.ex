d6e5925c1a1abef20c4ff6c2e30cc816e1353fa5
MmmarRTHa <m27842n@outlook.com>
2025-05-04 01:34:21 -0700

    - update: feature/user_accounts_&_auth - fix tests after username changes

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -6,11 +6,13 @@ defmodule Neighborly.AccountsFixtures do
 
   def unique_user_email, do: "user#{System.unique_integer()}@example.com"
   def valid_user_password, do: "hello world!"
+  def unique_user_username, do: "user#{:rand.uniform(9000) + 999}"
 
   def valid_user_attributes(attrs \\ %{}) do
     Enum.into(attrs, %{
       email: unique_user_email(),
-      password: valid_user_password()
+      password: valid_user_password(),
+      username: unique_user_username()
     })
   end
 
