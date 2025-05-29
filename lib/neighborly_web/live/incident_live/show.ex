bdf8826ab38858e3a14fb7c33f088097d8b6a482
MmmarRTHa <m27842n@outlook.com>
2025-05-28 18:25:16 -0700

    - update: feature/presence_tracking - add presence tracking in incident live view and integrate real-time onlooker updates

--- lib/neighborly_web/live/incident_live/show.ex
+++ lib/neighborly_web/live/incident_live/show.ex
@@ -1,4 +1,5 @@
 defmodule NeighborlyWeb.IncidentLive.Show do
+  alias NeighborlyWeb.Presence
   use NeighborlyWeb, :live_view
 
   alias Neighborly.Incidents
@@ -17,9 +18,24 @@ defmodule NeighborlyWeb.IncidentLive.Show do
   end
 
   def handle_params(%{"id" => id}, _uri, socket) do
+    %{current_user: current_user} = socket.assigns
+
     if connected?(socket) do
       Incidents.subscribe(id)
+
+      if current_user do
+        {:ok, _} =
+          Presence.track(self(), topic(id), current_user.username, %{
+            online_at: System.system_time(:second)
+          })
+      end
     end
+
+    presences =
+      Presence.list(topic(id))
+      |> Enum.map(fn {username, %{metas: metas}} ->
+        %{id: username, metas: metas}
+      end)
 
     incident = Incidents.get_incident!(id)
 
@@ -29,6 +45,7 @@ defmodule NeighborlyWeb.IncidentLive.Show do
       socket
       |> assign(:incident, incident)
       |> stream(:responses, responses)
+      |> stream(:presences, presences)
       |> assign(:response_count, Enum.count(responses))
       |> assign(:page_title, incident.name)
       |> assign_async(:urgent_incidents, fn ->
@@ -36,6 +53,10 @@ defmodule NeighborlyWeb.IncidentLive.Show do
       end)
 
     {:noreply, socket}
+  end
+
+  defp topic(incident_id) do
+    "incident_onlookers:#{incident_id}"
   end
 
   def render(assigns) do
@@ -87,10 +108,25 @@ defmodule NeighborlyWeb.IncidentLive.Show do
         </div>
         <div class="right">
           <.urgent_incidents incidents={@urgent_incidents} />
+          <.onlookers :if={@current_user} presences={@streams.presences} />
         </div>
       </div>
       <.back navigate={~p"/incidents"}>All Incidents</.back>
     </div>
+    """
+  end
+
+  def onlookers(assigns) do
+    ~H"""
+    <section>
+      <h4>Onlookers</h4>
+      <ul class="presences" id="onlookers" phx-update="stream">
+        <li :for={{dom_id, %{id: username, metas: metas}} <- @presences} id={dom_id}>
+          <.icon name="hero-user-circle-solid" class="w-5 h-5" />
+          {username} ({length(metas)})
+        </li>
+      </ul>
+    </section>
     """
   end
 
